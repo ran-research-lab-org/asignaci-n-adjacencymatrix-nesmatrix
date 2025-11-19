a73f7cd23acf891ca07a28489896ff5bc4927e53
Néstor Martínez <nestorandres04@gmail.com>
2025-11-19 17:43:37 +0000

    lab finished

--- Graph.hpp
+++ Graph.hpp
@@ -33,16 +33,30 @@ public:
     // Implementar!! 
     // Devuelve la cantidad de aristas
     int numEdges() const {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                if (adjMatrix[i][j] == 1) count++;
+            }
+        }
+
+        return count;
     }
 
     // Completa esta función
     // Devuelve el in-degree de un vertice
     int inDegree(int u) const {
+        int count = 0;
+
         if (u < 0 || u >= numVertices)
             throw std::out_of_range("Vertice fuera de rango");
         else {
+            for (int i = 0; i < numVertices; i++) {
+                if (adjMatrix[i][u] == 1) count++;
+            }
         }
+
+        return count;
     }
 
     // Completa esta función
@@ -50,6 +64,13 @@ public:
     // En caso de que haya varios nodos que tengan el mayor inDegree,
     // devuelve true si u es uno de ellos
     bool isInfluencer(int u) const  {
+        int uDegree = inDegree(u);
+
+        for (int i = 0; i < numVertices; i++) {
+            if (inDegree(i) > uDegree) return false;
+        }
+
+        return true;
     }
 };
 
